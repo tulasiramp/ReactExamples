de854261a36756196ac522fa1c0cca1ecd99acd3
tulasiram <tulasiram@srivatech.com>
2016-08-10 02:10:03 +0530

    final commit ofVehicle card UI component.

--- ReactDemo/ReactDemo/Scripts/VehicleDetails.jsx
+++ ReactDemo/ReactDemo/Scripts/VehicleDetails.jsx
@@ -2,27 +2,25 @@
 var VehicleCardUIComponent = React.createClass({
     render : function (){
 
-        return (
-            <div className='item1'>
-            <VehicleHeadSubComponent/>
-        <CRButtonSubComponent/>
-        <BodySubComponent/>
-        <FooterActionsSubComponent/>
-        </div>
-        );
+        return (<div>
+                <VehicleHeadSubComponent/>
+                <CRButtonSubComponent/>
+                <BodySubComponent/>
+                <FooterActionsSubComponent/>
+                </div> );
     }
 });
 var VehicleHeadSubComponent = React.createClass({
     render : function () {
         var style={
-            background:'royalblue',
+            background:'#6492BD',
             color : 'white',
             padding:'1px',
 			width:'240px'
 			};
 		
     return (
-		<div className="row"  style = {style}>
+		<div className="heading"  style = {style}>
 	        <div className="col-sm-4">
                 <p id="header">2011 CHEVROLET</p>
                 <p id="header">CORVETIE PREMIUM L</p>
@@ -37,33 +35,27 @@ var CRButtonSubComponent = React.createClass({
     render : function () {
 
         return (
-            <div className='item3'>
-               <span><img src="/Content/Images/Vehicle_Image.png"/></span>
-			   <div id="overlay">
-			    <span className="fa fa-eye" id='eye'></span>
-                <span className="fa fa-list" id='list'></span>
-                <button type = "button" id="button-small" value = "submit">CR</button>
-			   </div>
-			
-            </div>
-        );
+                <div className='item3'>
+                    <span><img src="/Content/Images/Vehicle_Image.png"/></span>
+                    <span id="number">#10032</span>
+			        <span>
+			            <button type = "button" id="button-small" value = "submit">CR</button>
+			        </span>
+		        </div> );
     }
 });
 
 
 var FooterActionsSubComponent = React.createClass({
     render : function () {
-
-        return (
+    return (
             <div className='item5' id="transbox">
-             <span>
-                <button type = "button" value = "submit" id='button'><span className="fa fa-info-circle"></span><br/>Detials</button>
-                <button type = "button" value = "submit" id='button'><span className="fa fa-plus"></span><br/>Add</button>
-                <button type = "button" value = "submit" id='button'><span className="fa fa-money"></span><br/>Bid/Buy</button>
-
-            </span>
-        </div>
-        );
+             <div id="bottom">
+                <span className="fa fa-eye" id="eye"><br/><a href=""><span id="watchlist">Watchlist</span></a></span>
+                <span className="fa fa-list" id="list"><br/><a href=""><span id="whishlist">Whishlists</span></a></span>
+                <span className="fa fa-money" id="money"><br/><a href=""><span id="bid">Bid/Buy</span></a></span>
+            </div>
+        </div> );
     }
 });
 
@@ -78,7 +70,7 @@ var BodySubComponent = React.createClass({
     componentDidMount: function() {
 
         var xhr = new XMLHttpRequest();
-        xhr.open('get', 'https://api.myjson.com/bins/52urd', true);
+        xhr.open('get', 'https://api.myjson.com/bins/z12t', true);
         xhr.onload = function () {
             var data = JSON.parse(xhr.responseText);
             console.log(data);
@@ -94,19 +86,14 @@ var BodySubComponent = React.createClass({
 
     render: function() {
         return (
-
             <div className='item4'>
-                <span key={this.state.Vehicle1.vehicle_id}>
-                          {this.state.Vehicle1.vehicle_id}<br/>
-                 Miles:   {this.state.Vehicle1.miles}<br/>
-                 Color:   {this.state.Vehicle1.color}<br/>
-                 Seller:  {this.state.Vehicle1.seller}<br/>
-				          {this.state.Vehicle1.duration}<br/>
-                 Buy Now: {this.state.Vehicle1.buynow}
-                </span>
-            </div>
-
-    );
+                <span id="buy">Buy :<span id="buynow">{this.state.Vehicle1.buynow}</span> </span>
+                <span id="highBid">High Bid: <span id="highid">{this.state.Vehicle1.high_bid}</span></span><br />
+                <span id="miles">Miles: {this.state.Vehicle1.miles}</span>
+                <span id="vehicle_color">Color:<span id="colorid">{this.state.Vehicle1.color}</span></span><br />
+                <span id="seller">Seller: <span id="vehicle_seller">{this.state.Vehicle1.seller}</span></span> <br />
+                <span id="duration"> {this.state.Vehicle1.duration}</span><br/>
+            </div>);
     }
 });
 
